f2ec727624fa232f89bfa0b72396b1cc915b7a43
anacarolinyzz <freefireana23@gmail.com>
2025-12-05 09:39:17 -0300

    mudei so um pouco

--- aula 5 js/aula5.js
+++ aula 5 js/aula5.js
@@ -28,14 +28,14 @@ const carregar = document.getElementById('carregar');
 const carregado = document.getElementById('carregado');
 salvar.addEventListener('click', () => {
     localStorage.setItem('usuarios', JSON.stringify(pessoas));
-    salvo.textContent = 'salvo';
-    salvo.style.color = 'green';
-    
+        salvo.textContent = 'salvo';
+        salvo.style.color = 'green';
+        carregar.addEventListener('click', () => {
+        const dados = JSON.parse(localStorage.getItem("pessoas"));
+        console.log(dados[0].nome);
+        carregado.textContent = 'carregado';
+        carregado.style.color = 'green';
+    });
 });
-carregar.addEventListener('click', () => {
-    const dados = JSON.parse(localStorage.getItem("pessoas"));
-    console.log(dados[0].nome);
-    carregado.textContent = 'carregado';
-    carregado.style.color = 'green';
-});
+
 
